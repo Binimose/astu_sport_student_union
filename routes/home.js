206134b9fef6d22043ccd568c8220bbd017c6210
Binimose <134205092+Binimose@users.noreply.github.com>
2025-11-12 04:40:15 -1000

    update: base route for the api

---
+++ routes/home.js
@@ -0,0 +1,12 @@
+// routes/match.js
+const express = require('express');
+const router = express.Router();
+ 
+router.get('/', async(req,res)=>{
+    return {
+        message
+:"hello"    }
+    
+});                    // Anyone can view
+ 
+module.exports = router;
